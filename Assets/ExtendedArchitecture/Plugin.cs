37acc842f26753c1e380964da8b281348cb74ac3
TheElec <really.elec@pm.me>
2022-06-19 19:38:38 +0200

    Added support for Experimental, Migrated to TimberAPI

--- Assets/ExtendedArchitecture/Plugin.cs
+++ Assets/ExtendedArchitecture/Plugin.cs
@@ -1,41 +1,71 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Numerics;
-using System.Reflection;
 using BepInEx;
-using HarmonyLib;
+using BepInEx.Logging;
+using Bindito.Core;
 using Timberborn.AssetSystem;
-using Timberborn.BlockSystem;
-using Timberborn.BlockSystemNavigation;
-using Timberborn.Buildings;
-using Timberborn.EntitySystem;
-using Timberborn.FactionSystemGame;
-using Timberborn.Goods;
-using Timberborn.Localization;
+using Timberborn.SingletonSystem;
+using TimberbornAPI;
+using TimberbornAPI.AssetLoaderSystem.AssetSystem;
+using TimberbornAPI.Common;
+using TimberbornAPI.LocalizationSystem;
 using UnityEngine;
-using Object = UnityEngine.Object;
-using Vector3 = UnityEngine.Vector3;
+using Logger = UnityEngine.Logger;
 
 namespace Elec.ExtendedArchitecture
 {
-    [BepInPlugin("Elec.ExtendedArchitecture", "Extended Architecture", "0.2.0")]
+    [BepInPlugin(PluginGuid, PluginGuid, PluginVersion)]
+    [BepInDependency("com.timberapi.timberapi")]
     public class Plugin : BaseUnityPlugin
     {
-        private static bool _hasLoaded = false;
-        private static AssetBundle _assetBundle;
+        public const string PluginGuid = "Elec.ExtendedArchitecture";
+        public const string PluginVersion = "0.3.0";
+
+        public static ManualLogSource Log; 
+        
         private void Awake()
         {
-            _assetBundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Info.Location), "elec.extendedarchitecture.bundle"));
-            if (_assetBundle == null) throw new Exception("AssetBundle not found!");
+            Log = Logger;
             
-            Harmony.CreateAndPatchAll(typeof(Plugin));
+            TimberAPI.AssetRegistry.AddSceneAssets(PluginGuid, SceneEntryPoint.Global);
             
-            Logger.LogInfo($"Loaded!");
+            TimberAPI.DependencyRegistry.AddConfiguratorBeforeLoad(new BuildingAddConfigurator(), SceneEntryPoint.MainMenu);
+            
+            Log.LogInfo($"Loaded {PluginGuid} {PluginVersion}!");
+        }
+    }
+
+    public class BuildingAdd : IInitializableSingleton
+    {
+        private readonly IAssetLoader _assetLoader;
+        private readonly IResourceAssetLoader _resourceAssetLoader;
+        
+        public BuildingAdd(IAssetLoader assetLoader, IResourceAssetLoader resourceAssetLoader)
+        {
+            _assetLoader = assetLoader;
+            _resourceAssetLoader = resourceAssetLoader;
+        }
+        
+        public void Initialize()
+        {
+            var platformModel = _resourceAssetLoader.Load<GameObject>("Buildings/Paths/Platform/Platform.Full.Folktails");
+            var shader = platformModel.GetComponent<MeshRenderer>().materials[0].shader;
+
+            AddBuilding("4x1arch", shader);
+            AddBuilding("5x1arch", shader);
+            
+            Plugin.Log.LogInfo($"Loaded buildings!");
         }
 
-        static void FixMaterialShader(GameObject obj, Shader shader)
+        private void AddBuilding(String name, Shader shader)
+        {
+            var building = _assetLoader.Load<GameObject>(Plugin.PluginGuid, $"elec.extendedarchitecture.bundle/{name}");
+            
+            FixMaterialShader(building, shader);
+            TimberAPI.CustomObjectRegistry.AddGameObject(building);
+        }
+        
+        private static void FixMaterialShader(GameObject obj, Shader shader)
         {
             var meshRenderer = obj.GetComponent<MeshRenderer>();
             if (meshRenderer)
@@ -54,82 +84,13 @@ namespace Elec.ExtendedArchitecture
                 }
             }
         }
+    }
 
-        private static Dictionary<string, GameObject> _moddedBuildings = new();
-
-        static void AddBuilding(string path, Shader shader, GoodAmount[] goodAmounts)
+    public class BuildingAddConfigurator : IConfigurator
+    {
+        public void Configure(IContainerDefinition containerDefinition)
         {
-            var building = _assetBundle.LoadAsset<GameObject>(path);
-            var prefabName = building.GetComponent<Prefab>().PrefabName;
-            building.GetComponent<Building>().BuildingCost = goodAmounts;
-            FixMaterialShader(building, shader);
-            LoadLocalization(building);
-            _moddedBuildings.Add(prefabName, building);
-        }
-
-        [HarmonyPatch(typeof(FactionObjectCollection), "GetObjects")]
-        [HarmonyPostfix]
-        static void FactionObjectCollection_GetObjects_Postfix(ref IEnumerable<Object> __result, FactionObjectCollection __instance)
-        {
-            if (!_hasLoaded)
-            {
-                var assetLoaderInfo = typeof(FactionObjectCollection).GetField("_resourceAssetLoader", 
-                    BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static );
-                if (assetLoaderInfo == null)
-                {
-                    throw new Exception("_resourceAssetLoader doesn't exist!");
-                }
-                var assetLoader = (IResourceAssetLoader) assetLoaderInfo.GetValue(__instance);
-
-                var platformModel = assetLoader.Load<GameObject>("Buildings/Paths/Platform/Platform.Full.Folktails");
-                var shader = platformModel.GetComponent<MeshRenderer>().materials[0].shader;
-
-                var logSpec = assetLoader.Load<GoodSpecification>("Goods/LogSpecification");
-                var plankSpec = assetLoader.Load<GoodSpecification>("Goods/PlankSpecification");
-                var metalSpec = assetLoader.Load<GoodSpecification>("Goods/MetalBlockSpecification");
-
-                AddBuilding("4x1Arch", shader,
-                    new []{
-                        new GoodAmount(logSpec, 8), 
-                        new GoodAmount(plankSpec, 8)
-                    } );
-                AddBuilding("5x1Arch", shader,
-                    new []{
-                        new GoodAmount(logSpec, 4), 
-                        new GoodAmount(plankSpec, 10),
-                        new GoodAmount(metalSpec, 6)
-                    } );
-                AddBuilding("Stairwell", shader, new GoodAmount[0] );
-
-                _hasLoaded = true;
-            }
-            __result = __result.Concat(_moddedBuildings.Values);
-        }
-
-        [HarmonyPatch(typeof(PrefabNameMapper), "GetPrefab", new [] {typeof(string)})]
-        [HarmonyPrefix]
-        static bool PrefabNameMapper_GetPrefab_Prefix(ref GameObject __result, string prefabName)
-        {
-            return !_moddedBuildings.TryGetValue(prefabName, out __result);
-        }
-        
-        private static Dictionary<string, string> _moddedLocalization = new();
-        
-        [HarmonyPatch(typeof(Loc), "T", new [] {typeof(string)})]
-        [HarmonyPrefix]
-        static bool Loc_T_Prefix(ref string __result, string key)
-        {
-            return !_moddedLocalization.TryGetValue(key, out __result);
-        }
-
-        static void LoadLocalization(GameObject obj)
-        {
-            var localization = obj.GetComponent<Localization>();
-            if (localization == null) return;
-            
-            _moddedLocalization.Add(localization.DisplayNameLocKey, localization.displayNameText);
-            _moddedLocalization.Add(localization.DescriptionLocKey, localization.descriptionText);
-            _moddedLocalization.Add(localization.FlavorDescriptionLocKey, localization.flavorText);
+            containerDefinition.Bind<BuildingAdd>().AsSingleton();
         }
     }
 }
